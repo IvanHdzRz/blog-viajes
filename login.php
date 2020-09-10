aef5149c7d270e749b87d73030a6cd80e988443c
Ivan Hernandez Ruiz <musedelick70@gmail.com>
2020-09-10 18:28:27 -0500

    se corrigieron estilos de los form login-signup

--- login.php
+++ login.php
@@ -5,18 +5,18 @@
     include_once './includes/navigation.php';
 ?>
 <div class="login">
-    <div class="container">
-        <form class="login-form">
-            <h1>Login</h1>
-            <p>it's a pleasure to see you again</p>
-            <input type="text" placeholder="user name" class="input-text"/>
-            <input type='password' placeholder="password"  class="input-text"/>
-            <div class="sign-up">
-                <a href="signup.html" class='redirect-signup-sign-in'>create account</a>
-                <input type='submit' value='sign in' class="btn primary" />
-            </div>
+    
+    <form class="login-form">
+        <h1>Login</h1>
+        <p>it's a pleasure to see you again</p>
+        <input type="text" placeholder="user name" class="input-text"/>
+        <input type='password' placeholder="password"  class="input-text"/>
+        <div class="sign-up">
+            <a href="signup.html" class='redirect-signup-sign-in'>create account</a>
+            <input type='submit' value='sign in' class="btn primary" />
+        </div>
             
-        </form>
-    </div>
+    </form>
+    
 </div>
 </html>
